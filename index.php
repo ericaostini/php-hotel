fb4cb8d5deb2e06e5c7cdffd342873464a9d5c98
ericaostini <ericaostini14@gmail.com>
2023-11-29 14:45:42 +0100

    display info hotel with foreach

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
 include __DIR__ . '/partials/header.php';
 ?>
-<main>
 
+
+<main class="container">
+    <div>
+        <?php foreach ($hotels as $hotel) { ?>
+            <p>
+                <?php echo $hotel["name"] ?>
+            </p>
+            <p>
+                <?php echo $hotel["description"] ?>
+            </p>
+            <p>
+                <?php echo $hotel["vote"] ?>
+            </p>
+            <p>
+                <?php echo $hotel["distance_to_center"] ?>
+            </p>
+            <span class="btn btn-success">
+                <?php echo $hotel["parking"] ?>
+            </span>
+        <?php } ?>
+    </div>
 </main>
 
 <?php
